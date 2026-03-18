3455a7849f48a86ab1cc609a5d8d89128863412b
hsh <74010090+dev-hsonghart@users.noreply.github.com>
2026-03-18 13:51:35 +0900

    jsx 기본 문법 실습

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,15 @@ import './App.css';
 
 function App() {
   let name = '테토포';
+
+  {
+    /* 
+		작성자 : 누구누구
+		작성일 : 2020~~
+		내용 : 기능에 대한 간략 내용
+ */
+  }
+
   return (
     <div className="App">
       <header className="App-header">안녕하세요 리액트 초짜입니다</header>
@@ -11,6 +20,7 @@ function App() {
         병아리 개발자 {name === '테토포' ? <h1>{name}</h1> : null}에요
       </p>
     </div>
+    // 주석을 사용합니다.
   );
 }
 
