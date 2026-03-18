cb68b6dd0a4bc362e2961746c8aebdd35648617b
hsh <74010090+dev-hsonghart@users.noreply.github.com>
2026-03-18 13:36:18 +0900

    조건문 사용

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
+  let name = '테토포';
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header">안녕하세요 리액트 초짜입니다</header>
+      <p className="App-content">
+        병아리 개발자 {name === '테토포' ? <h1>{name}</h1> : null}에요
+      </p>
     </div>
   );
 }
